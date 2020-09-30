8166116bd1e65da6f88ffdddd3b846f906c0ea9c
NCS99 <72143374+NCS99@users.noreply.github.com>
2020-09-30 19:31:10 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -16,16 +16,16 @@ else{
      uksort($data, 'strcasecmp');
 }
 // You can get the 'salt' from Instamojo's developers page(make sure to log in first): https://www.instamojo.com/developers
-// Pass the 'salt' without <>
+// Pass the 'SALT' without <>
 $mac_calculated = hash_hmac("sha1", implode("|", $data), "00c27972ca5c41e682aa0af55559df49");
 if($mac_provided == $mac_calculated){
     if($data['status'] == "Credit"){
-        // Payment was successful, mark it as successful in your database.
+        // Payment was successful, mark it as successful in your Database.
         // You can acess payment_request_id, purpose etc here. 
     }
     else{
-        // Payment was unsuccessful, mark it as failed in your database.
-        // You can acess payment_request_id, purpose etc here.
+        // Payment was unsuccessful, mark it as failed in your Database.
+        // You can acess payment_request_id, purpose etc Here.
     }
 }
 else{
